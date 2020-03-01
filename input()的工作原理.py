cf0dc3b5ad40091acc4205413b41dfafb129cff5
sumererer <53703362+sumererer@users.noreply.github.com>
2020-03-01 15:18:47 +0800

    Add files via upload
    第一次上传

---
+++ input()的工作原理.py
@@ -0,0 +1,8 @@
+message = input("tell me something, and i will tell you:")
+print(message)
+name = input("please enter your name:")
+print("hello," + name + "!")
+prompt = "if you tell ue who you are, we can personalize the message you see."
+prompt += "\nWhat is your first name? "
+name = input(prompt)
+print("\nHello, " + name + "!")
